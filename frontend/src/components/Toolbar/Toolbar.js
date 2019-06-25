b0802a8fc828a830e6867f2ba05a47be7c12de0c
Felipe Angelim <felipeangelim@gmail.com>
2019-06-25 09:50:08 -0300

    Add responsive navbar

--- frontend/src/components/Toolbar/Toolbar.js
+++ frontend/src/components/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import './Toolbar.css';
-
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 const toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
-            <div></div>
+            <div className="toolbar_toggle-button">
+                <DrawerToggleButton click={props.drawerClickHandler}/>
+            </div>
             <div className="toolbar_logo"> <a href="/">THE LOGO</a></div>
+            <div className="spacer"></div>
             <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/">Products</a></li>
