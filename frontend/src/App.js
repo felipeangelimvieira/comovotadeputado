9c15cb7dad3ed63d779406bb1f954d658cea7d7f
Felipe Angelim <felipeangelim@gmail.com>
2019-06-25 14:37:18 -0300

    Add centered text to image background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,46 @@
 import React, {Component} from 'react';
 
-import Toolbar from './components/NavBar/Toolbar/Toolbar';
-import SideDrawer from './components/NavBar/SideDrawer/SideDrawer';
-import Backdrop from './components/NavBar/Backdrop/Backdrop';
+import {Image} from 'react-bootstrap';
 import NavBar from './components/NavBar/NavBar';
+import imagem_congresso from './assets/imagem_congresso.jpg'
 
 class App extends Component {
+
+  
   render() {
+
+  var background = { width: '100%',
+  backgroundSize: 'cover'};
+  var textStyle = { 
+    position: 'absolute',
+    top: '10vw',
+    'text-align' : 'center',
+    color: '#fff',
+    margin: '0 10% 0 10%',
+    'font-size' : '4vw',
+    'horizontal-align': 'middle',
+  }
+
   return (
     <div style={{height: '100%'}}>
       <NavBar />
-      <main style={{marginTop: '64px'}}>
-      <p>This is the page content</p>
+      <main style={{top: 0, left: 0, width : '100%'}}>
+        
+      <div style={{
+    width : '100%',
+    padding: 0,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+    display: 'inline',
+    'text-align': 'center',
+    border: 'none'}}>
+        <Image
+          style={background} responsive
+          src={imagem_congresso}>
+        </Image>
+        <h1 style={textStyle}>Voce sabe quem seu candidato foi durante as eleições. <br/> Quem ele é agora? </h1>
+      </div>
       </main>
     </div>
   );
