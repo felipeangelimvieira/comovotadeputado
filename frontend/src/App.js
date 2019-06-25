3415a2ccc7579362352eb7a397924de8f93fee80
Felipe Angelim <felipeangelim@gmail.com>
2019-06-25 10:13:55 -0300

    Set NavBar as a single component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,15 @@
 import React, {Component} from 'react';
 
-import Toolbar from './components/Toolbar/Toolbar';
-import SideDrawer from './components/SideDrawer/SideDrawer';
-import Backdrop from './components/Backdrop/Backdrop';
-
+import Toolbar from './components/NavBar/Toolbar/Toolbar';
+import SideDrawer from './components/NavBar/SideDrawer/SideDrawer';
+import Backdrop from './components/NavBar/Backdrop/Backdrop';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
-  state = {
-    sideDrawerOpen : false
-  };
-
-  drawerToggleClickHandler = () => {
-  
-    this.setState((prevState) => {
-      return {sideDrawerOpen: !prevState.sideDrawerOpen};
-    });
-  
-  };
-
-
-  backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false})
-  }
-
   render() {
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler}/>;
-    }
   return (
     <div style={{height: '100%'}}>
-      <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
-      <SideDrawer show={this.state.sideDrawerOpen} />
-      {backdrop}
+      <NavBar />
       <main style={{marginTop: '64px'}}>
       <p>This is the page content</p>
       </main>
---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+import Toolbar from './Toolbar/Toolbar';
+import SideDrawer from './SideDrawer/SideDrawer';
+import Backdrop from './Backdrop/Backdrop';
+
+class NavBar extends Component {
+    state = {
+      sideDrawerOpen : false
+    };
+  
+    drawerToggleClickHandler = () => {
+    
+      this.setState((prevState) => {
+        return {sideDrawerOpen: !prevState.sideDrawerOpen};
+      });
+    
+    };
+  
+  
+    backdropClickHandler = () => {
+      this.setState({sideDrawerOpen: false})
+    }
+  
+    render() {
+      let backdrop;
+  
+      if (this.state.sideDrawerOpen) {
+        backdrop = <Backdrop click={this.backdropClickHandler}/>;
+      }
+    return (
+        <div className="navbar">
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        </div>
+    );
+  }
+  }
+
+  export default NavBar
