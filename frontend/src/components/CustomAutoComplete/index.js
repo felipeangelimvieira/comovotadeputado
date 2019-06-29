17fa3caba7eda586bfd05dd8a122c8dd69d94ae1
Felipe Angelim <felipeangelim@gmail.com>
2019-06-29 01:08:52 -0300

    Menu highlight style

--- frontend/src/components/CustomAutoComplete/index.js
+++ frontend/src/components/CustomAutoComplete/index.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Autocomplete from 'react-autocomplete';
 
+function toCamelCase(string) {
+
+  var split = string.split(' ');
+  var new_word = [];
+  for (var i = 0; i < split.length; i++) {
+    var word = split[i];
+    if (word === 'de' || word === 'das' || word === 'dos') {
+      new_word.push(word);
+    }
+    else {
+      new_word.push(word[0].toUpperCase() + word.slice(1));
+    }
+  }
+
+  return new_word.join(' ')
+
+}
+
 function matchItemToValue(item, value) {
     return (
       item.nomeParlamentar.toLowerCase().indexOf(value.toLowerCase()) !== -1 
@@ -10,9 +28,11 @@ function matchItemToValue(item, value) {
 class CustomAutocomplete extends Component {
     state = { value: '' } 
     render() {
+      console.log(toCamelCase('abc de eftgh asdipij'));
+      console.log(this.props.items)
       return (
         <div>
-          <label htmlFor="states-autocomplete">Choose a state from the US</label>
+          <label htmlFor="states-autocomplete">Como vota,</label>
           <Autocomplete
             value={this.state.value}
             //inputProps={{ id: 'states-autocomplete' }}
@@ -28,9 +48,9 @@ class CustomAutocomplete extends Component {
               </div>
             )}
             renderItem={(item, isHighlighted) => (
-              <div
+              <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}
                 className={`item ${isHighlighted ? 'item-highlighted' : ''}`}
-                //key={item.id}
+                key={item.deputado_id}
               >{item.nomeParlamentar}</div>
             )}
           />
